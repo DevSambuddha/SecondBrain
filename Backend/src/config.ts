c19ce786ad03d7535b9a6a1e1232064476f46cee
Sambuddha Biswas <38996553+XiLucas@users.noreply.github.com>
2025-04-29 19:01:12 +0530

    edited config

--- Backend/src/config.ts
+++ Backend/src/config.ts
@@ -3,4 +3,4 @@ export const MONGODB_USER = process.env.mongo_username || "";
 export const MONGODB_PASSWORD = process.env.mongo_password || "";
 export const MONGODB_DB = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@secondbrain.zxd0dxn.mongodb.net/SecondBrain`;
 
-export const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+export const SERVER_PORT = process.env.PORT;
